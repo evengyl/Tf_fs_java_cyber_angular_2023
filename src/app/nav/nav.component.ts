a2c7e19150c1f44fc88be847a6d612e6333a8073
Evengyl <dark.evengyl@gmail.com>
2023-09-04 14:33:17 +0200

    add ng docheck hooks

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, inject } from '@angular/core';
+import { GuardsAuthService } from '../demo/guards/guards-auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html'
 })
-export class NavComponent {
+export class NavComponent implements DoCheck{
+
+  isNavigationAllowed : boolean = false
+
+  constructor(private auth : GuardsAuthService){
+
+  }
+
+  ngDoCheck(): void {
+    console.log("Checked")
+    //Attention le doCheck demédie a ne pas devoir faire des OBS partout... mais est appele TOUT LE TEMPS POUR TOUT !!!
+    //un hover, une directive, un click etc.... donc des centaine de fois par minutes de navigation...
+    //a utiliser avec la plus grande prudence et parcimonie
+    // throw new Error('Method not implemented.');
+    
+ 
+
+    this.isNavigationAllowed = this.auth.isConnectGuard as boolean
+
+  }
 }
